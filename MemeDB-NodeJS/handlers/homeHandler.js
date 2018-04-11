f303a4218129402fb3aaa417a5379b45a1fca16b
Sevgin Mustafov <sevgin95@gmail.com>
2018-04-11 13:19:57 +0300

    Initial Commit

---
+++ MemeDB-NodeJS/handlers/homeHandler.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const filePath = './views/home.html'
+const utils = require('../utils/default')
+
+module.exports = (req, res) => {
+  if (req.pathname === '/' && req.method === 'GET') {
+    fs.readFile(filePath, (err, data) => {
+        if(err){
+            utils.defaultErrorRes(res, err)
+            return
+        }
+        utils.defaultSuccessRes(res, data)
+    })
+  } else {
+      return true
+  }
+}
---
+++ MemeDB-NodeJS/handlers/memeHandler.js
@@ -0,0 +1,112 @@
+const fs = require('fs')
+const db = require('../config/dataBase')
+const viewAllPath = './views/viewAll.html'
+const viewAddMemePath = './views/addMeme.html'
+const detailsPath = './views/details.html'
+const utils = require('../utils/default')
+const qs = require('query-string')
+const formidable = require('formidable')
+const shortid = require('shortid')
+
+let viewAll = (req, res) => {
+  let memes = db.getDb()
+
+  fs.readFile(viewAllPath, 'utf8', (err, data) => {
+    if (err) {
+      utils.defaultErrorRes(res, err)
+      return
+    }
+
+    let html = utils.generateHtmlBody(memes, data)
+    
+    utils.defaultSuccessRes(res, html)
+  })
+}
+
+let viewAddMeme = (req, res) => {
+  fs.readFile(viewAddMemePath, (err, data) => {
+    if(err) {
+      utils.defaultErrorRes(res, err)
+      return
+    }
+
+    utils.defaultSuccessRes(res, data)
+  })
+}
+
+let addMeme = (req, res) => {
+  let currentMeme = {}
+  let form = new formidable.IncomingForm()
+
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      utils.defaultErrorRes(res, err)
+      return
+    }
+
+    let imagePath = files.meme.path
+    let imageExtIndex = files.meme.name.lastIndexOf('.')
+    let imgExt = files.meme.name.substr(imageExtIndex, files.meme.name.length - 1)
+
+    fs.readFile(image, 'binary', (err, data) => {
+      if(err) {
+        utils.defaultErrorRes(res, err)
+        return
+      }
+
+      let imgId = shortid.generate()
+      let savedImagePath = `./public/memeStorage/0/`
+
+      fs.writeFile(savedImagePath + imgId + imgExt, data, 'binary', (err) => {
+        if(err) {
+          utils.defaultErrorRes(res, err)
+        }
+
+        let id = shortid.generate()
+
+        currentMeme['id'] = id
+        currentMeme['title'] = fields['memeTitle']
+        currentMeme['memeSrc'] = savedImagePath + imgId + imgExt
+        currentMeme['description'] = fields['memeDescription']
+        currentMeme['privacy'] = fields['status']
+        currentMeme['timeStamp'] = Date.now()
+        
+        db.add(currentMeme)
+        
+        res.writeHead(302, { 'location': '/viewAllMemes'})
+        res.end()
+      })
+    })
+  })
+}
+
+let getDetails = (req, res) => {
+  let memeId = qs.parse(qs.extract(req.url))['id']
+  let currentDb = db.getDb()
+  let currentMeme = currentDb.find(m => m.id === memeId)
+
+  fs.readFile(detailsPath, 'utf8', (err, data) => {
+    if (err) {
+      utils.defaultErrorRes(res, err)
+      return
+    }
+
+    let html = utils.generateDetailsHtml(currentMeme, data)
+    
+    utils.defaultSuccessRes(res, html)
+  })
+}
+
+module.exports = (req, res) => {
+  if (req.pathname === '/viewAllMemes' && req.method === 'GET') {
+    viewAll(req, res)
+  } else if (req.pathname === '/addMeme' && req.method === 'GET') {
+    viewAddMeme(req, res)
+  } else if (req.pathname === '/addMeme' && req.method === 'POST') {
+    addMeme(req, res)
+  } else if (req.pathname.startsWith('/getDetails') && req.method === 'GET') {
+    getDetails(req, res)
+  } else {
+    return true
+  }
+}
